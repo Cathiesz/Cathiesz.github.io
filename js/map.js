f60388c26f2573e7a38b38b62ad61b6f89627943
Katharina Biernacka <kath.biernacka@gmail.com>
2021-12-05 16:38:27 +0100

    added eventhandler

---
+++ js/map.js
@@ -0,0 +1,16 @@
+var i;
+
+function randomiser() {
+    return Math.random() * 6;
+}
+
+function showToRemove() {
+    if (myNumber(i) == randomiser()) {
+        makePartShake(i);
+    }
+}
+
+class Map {
+    remove(i) {
+    }
+}
--- js/searchForTreasure.js
+++ js/searchForTreasure.js
@@ -1,3 +1,40 @@
-function digging() {
-    
+
+var click;
+
+var map = document.querySelector('.map');
+var sensor = document.querySelector('.sensor');
+var maxX = map.clientWidth - sensor.clientWidth;
+var maxY = map.clientWidth - sensor.clientWidth;
+
+
+function digging(partOfMap) {
+    if (click == 10) {
+        digged = document.querySelector(partOfMap);
+        digged.update;
+        click = 0;
+    } else {
+        click++;
+    }
 }
+
+function handleOrientation(event) {
+
+    var y     = event.beta; // x axis
+    var x    = event.gamma; // y axis
+
+    output.textContent  = `beta : ${x}\n`;
+    output.textContent += `gamma: ${y}\n`;
+
+    // We dont want to move the device upside down
+    if (x >  90) { x =  90};
+    if (x < -90) { x = -90};
+
+    //shift range
+    x += 90;
+    y += 90;
+
+    ball.style.top  = (maxY*y/180 - 10) + "px";
+    ball.style.left = (maxX*x/180 - 10) + "px";
+}
+
+window.addEventListener('deviceorientation', handleOrientation);
